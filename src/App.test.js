bffff2bcf4da128e6a9613b014142c8f0afee67c
Bhabishya Gurung <bhab_ej@hotmail.com>
2023-04-13 22:39:36 +0300

    app test

--- src/App.test.js
+++ src/App.test.js
@@ -5,7 +5,7 @@ import Todolist from './components/Todolist';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { DemoContainer } from '@mui/x-date-pickers/internals';
-
+  
 test('renders todotable', () => {
 const row = [
 {description: 'Go to coffee', priority: 'high', date: '24.01.2021'}
